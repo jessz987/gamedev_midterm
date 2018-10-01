949f7352b5cf21135bdd436cb837b39fb63f1d63
Jess <zjess987@gmail.com>
2018-09-30 20:53:19 -0400

    30/9: initial prototype finished

--- Assets/Scripts/HangPoster.cs
+++ Assets/Scripts/HangPoster.cs
@@ -4,29 +4,69 @@ using UnityEngine;
 using UnityEngine.UI;
 
 
-// put this on a pole  (make sure it's unactive)
+// put this on a pole  (make sure related poster is unactive)
 // player will press E in pole trigger to make the poster on it active
 public class HangPoster : MonoBehaviour {
 
+	public GameObject poster;
+
+	private bool InPosterTrigger;
+	private bool CanHangPoster;
+	private bool PosterUp;
+	
 	public Text uiTextThought;
 	public Text uiTextControlHint;
-	public GameObject poster;
-	
-	
-	void OnTriggerEnter(Collider coll)
+
+	void Start()
 	{
-		uiTextThought.text = ("okay, I can hang up a poster here");
+		InPosterTrigger = false;
+		CanHangPoster = false;
+		PosterUp = false;
+	}
+
+	void Update()
+	{
+		// if player is in trigger, bool changed so can hang poster
+		if (InPosterTrigger)
+		{
+			if (Input.GetKey(KeyCode.E))
+			{
+				CanHangPoster = true;
+			}
+		}
 		
-		uiTextControlHint.gameObject.SetActive(true);
-		uiTextControlHint.text = ("press E to interact");
-		
-		// press E to hang up the poster (set the poster game object as active)
-		// put this in update instead, and then make setactive(false) in ontriggerexit
-		// here instead, make a boolean true that you then change in update!
-		if (Input.GetKey(KeyCode.E))
+		// if bool is true (from pressing E in trigger), poster goes up
+		// uiText is also updated, and player can no longer interact with the poster
+		if (CanHangPoster)
 		{
 			poster.gameObject.SetActive(true);
 			uiTextControlHint.gameObject.SetActive(false);
+			PosterUp = true;
+
+			
+			uiTextThought.text = "alright... next poster.";
 		}
+	}
+	
+	void OnTriggerEnter(Collider coll)
+	{
+		// once poster is up, can no longer interact with it
+		if (!PosterUp && GameManager.foodBowlPlaced)
+		{
+			uiTextThought.text = ("okay, I can hang up a poster here");
+		
+			uiTextControlHint.gameObject.SetActive(true);
+			uiTextControlHint.text = ("press E to interact");
+		}
+		
+		InPosterTrigger = true;
+	}
+
+	private void OnTriggerExit(Collider other)
+	{
+		CanHangPoster = false;
+		InPosterTrigger = false;
+		
+		GameManager.numPostersLeft--;
 	}
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // put this on a player with a RigidBody to move it
 // this script does mouse look and WASD movement
@@ -8,11 +9,24 @@ public class PlayerController : MonoBehaviour
 {
 	public float moveSpeed = 10f;
 	
+	public Text uiTextNumPosters;
+	public Text uiTextThought;
+	public Text uiTextControlHint;
+	
+	
 	// this variable will remember input and pass it to physics
 	private Vector3 inputVector;
+
+	void Start()
+	{
+		GameManager.numTotalPosters = 20;
+		GameManager.numPostersLeft = GameManager.numTotalPosters;
+	}
 	
 	
 	void Update () {
+		// UI always tells player # of posters left to put up
+		uiTextNumPosters.text = "posters left: " + GameManager.numPostersLeft.ToString();
 		
 		// MOUSE look: getting mouse input
 		float mouseX = Input.GetAxis("Mouse X"); // horizontal mouse input
@@ -28,6 +42,22 @@ public class PlayerController : MonoBehaviour
 		
 		inputVector = transform.forward * vertical; // forward
 		inputVector += transform.right * horizontal; // strafe
+		
+		// ENDING
+		// cheat code: P to skip to all 20 posters up
+		if (Input.GetKey(KeyCode.P))
+		{
+			GameManager.numPostersLeft = 0;
+			// once all posters all up, end game:
+			if (GameManager.numPostersLeft <= 0)
+			{
+				uiTextThought.text = "okay they're all up... i guess all i can do is go back home for now...";
+				uiTextNumPosters.gameObject.SetActive(false);
+				uiTextControlHint.gameObject.SetActive(false);
+			}
+		}
+		
+		
 	}
 	
 	void FixedUpdate()
